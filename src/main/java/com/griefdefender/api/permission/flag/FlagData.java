0eaf3b46459983eaec31757340c3c6fc64fde05f
bloodshot <jdroque@gmail.com>
2019-12-17 14:51:24 -0500

    Add FlagDefinition API.
    * Update build.gradle for shadowJar task.

---
+++ src/main/java/com/griefdefender/api/permission/flag/FlagData.java
@@ -0,0 +1,114 @@
+/*
+ * This file is part of GriefDefenderAPI, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) bloodmc
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.griefdefender.api.permission.flag;
+
+import java.util.Set;
+
+import com.griefdefender.api.GriefDefender;
+import com.griefdefender.api.permission.Context;
+
+/**
+ * Represents flag permission data stored in a {@link FlagDefinition}.
+ */
+public interface FlagData {
+
+    /**
+     * Gets the {@link Flag} associated with this data.
+     * 
+     * @return The flag
+     */
+    Flag getFlag();
+
+    /**
+     * Gets the {@link Context}'s associated with this data.
+     * 
+     * @return The contexts
+     */
+    Set<Context> getContexts();
+
+    /**
+     * Checks if {@link FlagData} matches this data.
+     * 
+     * @param flagData The flag data to check
+     * @return Whether this data matches
+     */
+    default boolean matches(FlagData flagData) {
+        if (flagData == null) {
+            return false;
+        }
+        return this.matches(flagData.getFlag(), flagData.getContexts());
+    }
+
+    /**
+     * Checks if a {@link Flag} with {@link Context}'s match this data.
+     * 
+     * @param flag The flag to check
+     * @param contexts The contexts to check
+     * @return Whether this data matches
+     */
+    boolean matches(Flag flag, Set<Context> contexts);
+
+    /**
+     * Gets a new claim builder instance for {@link Builder}.
+     * 
+     * @return A new claim builder instance
+     */
+    public static FlagData.Builder builder() {
+        return GriefDefender.getRegistry().createBuilder(FlagData.Builder.class);
+    }
+
+    public interface Builder {
+
+        /**
+         * The {@link Flag} associated with this data.
+         * 
+         * @param flag The flag
+         * @return The builder
+         */
+        Builder flag(Flag flag);
+
+        /**
+         * The {@link Context}'s associated with this data.
+         * 
+         * @param contexts The contexts
+         * @return The builder
+         */
+        Builder contexts(Set<Context> contexts);
+
+        /**
+         * Resets the builder to default settings.
+         * 
+         * @return The builder
+         */
+        Builder reset();
+
+        /**
+         * Returns the {@link FlagData}.
+         * 
+         * @return The flag data
+         */
+        FlagData build();
+    }
+}
---
+++ src/main/java/com/griefdefender/api/permission/flag/FlagDefinition.java
@@ -0,0 +1,206 @@
+/*
+ * This file is part of GriefDefenderAPI, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) bloodmc
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.griefdefender.api.permission.flag;
+
+import java.util.List;
+import java.util.Set;
+
+import com.griefdefender.api.CatalogType;
+import com.griefdefender.api.GriefDefender;
+import com.griefdefender.api.Tristate;
+import com.griefdefender.api.permission.Context;
+
+import net.kyori.text.Component;
+
+/**
+ * Represents a flag definition which can hold one or more {@link Flag} permissions.
+ */
+public interface FlagDefinition extends CatalogType {
+
+    /**
+     * Whether this definition is enabled.
+     * 
+     * @return true if enabled
+     */
+    boolean isEnabled();
+
+    /**
+     * Gets the group name associated with this flag definition.
+     * 
+     * @return The group name
+     */
+    String getGroupName();
+
+    /**
+     * Gets the friendly description.
+     * 
+     * @return The friendly description
+     */
+    String getFriendlyDescription();
+
+    /**
+     * Gets the default value.
+     *  
+     * @return The default value
+     */
+    Tristate getDefaultValue();
+
+    /**
+     * Gets the description.
+     * 
+     * @return The description
+     */
+    Component getDescription();
+
+    /**
+     * Gets the contexts associated with this definition.
+     * 
+     * @return The definition contexts
+     */
+    Set<Context> getContexts();
+
+    /**
+     * Gets the {@link FlagData} associated with this definition.
+     * 
+     * @return The flag data
+     */
+    List<FlagData> getFlagData();
+
+    /**
+     * Gets a list of {@link Flag}'s associated with this definition.
+     * 
+     * @return The list of flags
+     */
+    List<Flag> getFlags();
+
+    /**
+     * Adds {@link FlagData} to this definition.
+     * 
+     * @param flagData The flag data to add
+     */
+    void addFlagData(FlagData flagData);
+
+    /**
+     * Sets {@link Context}'s associated with this definition.
+     * 
+     * @param contexts
+     */
+    void setContexts(Set<Context> contexts);
+
+    /**
+     * Set to enable or disable this definition.
+     * 
+     * @param enabled
+     */
+    void setIsEnabled(boolean enabled);
+
+    /**
+     * Sets the default value of definition.
+     * 
+     * @param value The default value
+     */
+    void setDefaultValue(Tristate value);
+
+    /**
+     * Sets the group associated with this definition.
+     * 
+     * @param group The group name
+     */
+    void setGroupName(String group);
+
+    /**
+     * Gets a new claim builder instance for {@link Builder}.
+     * 
+     * @return A new claim builder instance
+     */
+    public static FlagDefinition.Builder builder() {
+        return GriefDefender.getRegistry().createBuilder(FlagDefinition.Builder.class);
+    }
+
+    public interface Builder {
+
+        /**
+         * Whether this definition is enabled.
+         * 
+         * @param enabled
+         * @return The builder
+         */
+        Builder enabled(boolean enabled);
+
+        /**
+         * Sets the default value.
+         * 
+         * @param value The default value
+         * @return The builder
+         */
+        Builder defaultValue(Tristate value);
+
+        /**
+         * Sets the flag data.
+         * 
+         * @param data The flag data
+         * @return The builder
+         */
+        Builder flagData(List<FlagData> data);
+
+        /**
+         * Sets the contexts.
+         * 
+         * @param contexts The contexts
+         * @return The builder
+         */
+        Builder contexts(Set<Context> contexts);
+
+        /**
+         * Sets the definition name.
+         * 
+         * @param name The definition name
+         * @return The builder
+         */
+        Builder name(String name);
+
+        /**
+         * Sets the description.
+         * 
+         * @param description The description
+         * @return The builder
+         */
+        Builder description(Component description);
+
+        /**
+         * Resets the builder to default settings.
+         * 
+         * @return The builder
+         */
+        Builder reset();
+
+        /**
+         * Returns the {@link FlagDefinition}.
+         * 
+         * @return The flag data
+         */
+        FlagDefinition build();
+    }
+}
